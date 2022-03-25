4a1aa34c7074775a2160c2b1f68c32bf77914fce
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2022-03-25 09:24:56 +0800

    Change some breeds image url for better display purpose

--- helpers/getbreeds.js
+++ helpers/getbreeds.js
@@ -1,3 +1,14 @@
+function changeImageUrl(oldData, breedId, newUrl) {
+	const newData = oldData.map((row) => {
+		if (row.id === breedId) {
+			const newRow = { ...row };
+			newRow.image.url = newUrl;
+			return newRow;
+		}
+		return row;
+	});
+	return newData;
+}
 export default async function getAllBreeds() {
 	const allBreeds = await fetch("https://api.thecatapi.com/v1/breeds", {
 		headers: {
@@ -5,5 +16,16 @@ export default async function getAllBreeds() {
 			"x-api-key": process.env.api_key,
 		},
 	});
-	return allBreeds.json();
+	let data = await allBreeds.json();
+	data = changeImageUrl(
+		data,
+		"beng",
+		"https://i.ibb.co/fCYGfT6/bodi-raw-q-rep-JO7x-PA-unsplash.jpg"
+	);
+	data = changeImageUrl(
+		data,
+		"srex",
+		"https://i.ibb.co/2kqGhWf/amber-kipp-75715-CVEJh-I-unsplash.jpg"
+	);
+	return data;
 }
--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = nextConfig;
 // next.config.js
 module.exports = {
 	images: {
-		domains: ["cdn2.thecatapi.com"],
+		domains: ["cdn2.thecatapi.com", "i.ibb.co"],
 	},
 };
